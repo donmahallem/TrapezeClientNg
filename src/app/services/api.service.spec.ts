5857d2ec7fed6c1a444103d3403d9ae11b6fd8c1
donmahallem <donmahallem@users.noreply.github.com>
2019-04-01 22:40:49 +0200

    tests and lint

--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -1,28 +1,28 @@
-import { TestBed, async } from "@angular/core/testing";
-import { ApiService } from "./api.service";
-import { HttpClient } from "@angular/common/http";
-import { Observable, of } from "rxjs";
-//import * as sinon from "sinon";
+import { HttpClient } from '@angular/common/http';
+import { async, TestBed } from '@angular/core/testing';
+import { of, Observable } from 'rxjs';
+import { ApiService } from './api.service';
+// import * as sinon from "sinon";
 describe('src/app/services/api.service', () => {
     describe('ApiService', () => {
         let apiService: ApiService;
         let getSpy: jasmine.Spy<InferableFunction>;
-        const testEndpoint: string = "https://test.com/";
+        const testEndpoint = 'https://test.com/';
         beforeAll(() => {
             getSpy = jasmine.createSpy();
         });
         beforeEach(async(() => {
-            getSpy.and.callFake((url: string): Observable<any> => {
-                return of(url);
+            getSpy.and.callFake((...args: any[]): Observable<any> => {
+                return of(args);
             });
             TestBed.configureTestingModule({
                 providers: [ApiService,
                     {
                         provide: HttpClient,
                         useValue: {
-                            get: getSpy
-                        }
-                    }]
+                            get: getSpy,
+                        },
+                    }],
             });
             apiService = TestBed.get(ApiService);
             spyOn(apiService, 'baseUrl').and.returnValue(testEndpoint);
@@ -34,11 +34,21 @@ describe('src/app/services/api.service', () => {
 
         describe('getTripPassages(tripId)', () => {
             it('should construct the request correctly', (done) => {
-                const testId: string = "testId1234";
+                const testId = 'testId1234';
                 apiService.getTripPassages(testId).subscribe((res) => {
-                    expect(res).toEqual(testEndpoint + 'api/trip/testId1234/passages?mode=departure');
+                    expect(res)
+                        .toEqual([testEndpoint + 'api/trip/testId1234/passages?mode=departure']);
                 }, done, done);
             });
-        })
+        });
+        describe('getRouteByVehicleId(vehicleId)', () => {
+            it('should construct the request correctly', (done) => {
+                const testId = 'testId1234';
+                apiService.getTripPassages(testId).subscribe((res) => {
+                    expect(res)
+                        .toEqual([testEndpoint + 'api/vehicle/testId1234/route']);
+                }, done, done);
+            });
+        });
     });
 });
