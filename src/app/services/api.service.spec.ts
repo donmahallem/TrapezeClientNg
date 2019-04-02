4080d57e44259b25687dd8f949e8c73b82f1c1ec
DonMahallem <donmahallem@users.noreply.github.com>
2019-04-02 09:39:31 +0200

    More tests

--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -8,6 +8,7 @@ describe('src/app/services/api.service', () => {
         let apiService: ApiService;
         let getSpy: jasmine.Spy<InferableFunction>;
         const testEndpoint = 'https://test.com/';
+        const testId = 'testId1234';
         beforeAll(() => {
             getSpy = jasmine.createSpy();
         });
@@ -34,19 +35,57 @@ describe('src/app/services/api.service', () => {
 
         describe('getTripPassages(tripId)', () => {
             it('should construct the request correctly', (done) => {
-                const testId = 'testId1234';
                 apiService.getTripPassages(testId).subscribe((res) => {
                     expect(res)
-                        .toEqual([testEndpoint + 'api/trip/testId1234/passages?mode=departure']);
+                        .toEqual([testEndpoint + 'api/trip/' + testId + '/passages?mode=departure']);
                 }, done, done);
             });
         });
         describe('getRouteByVehicleId(vehicleId)', () => {
             it('should construct the request correctly', (done) => {
-                const testId = 'testId1234';
-                apiService.getTripPassages(testId).subscribe((res) => {
+                apiService.getRouteByVehicleId(testId).subscribe((res) => {
                     expect(res)
-                        .toEqual([testEndpoint + 'api/vehicle/testId1234/route']);
+                        .toEqual([testEndpoint + 'api/vehicle/' + testId + '/route']);
+                }, done, done);
+            });
+        });
+        describe('getRouteByTripId(vehicleId)', () => {
+            it('should construct the request correctly', (done) => {
+                apiService.getRouteByTripId(testId).subscribe((res) => {
+                    expect(res)
+                        .toEqual([testEndpoint + 'api/trip/' + testId + '/route']);
+                }, done, done);
+            });
+        });
+        describe('getStopInfo(vehicleId)', () => {
+            it('should construct the request correctly', (done) => {
+                apiService.getStopInfo(testId).subscribe((res) => {
+                    expect(res)
+                        .toEqual([testEndpoint + 'api/stop/' + testId + '/info']);
+                }, done, done);
+            });
+        });
+        describe('getStopDepartures(vehicleId)', () => {
+            it('should construct the request correctly', (done) => {
+                apiService.getStopDepartures(testId).subscribe((res) => {
+                    expect(res)
+                        .toEqual(<any>[testEndpoint + 'api/stop/' + testId + '/departures']);
+                }, done, done);
+            });
+        });
+        describe('getVehicleLocation(vehicleId)', () => {
+            it('should construct the request correctly', (done) => {
+                apiService.getVehicleLocation(testId).subscribe((res) => {
+                    expect(res)
+                        .toEqual([testEndpoint + 'api/geo/vehicle/testId1234']);
+                }, done, done);
+            });
+        });
+        describe('getStations()', () => {
+            it('should construct the request correctly', (done) => {
+                apiService.getStations().subscribe((res) => {
+                    expect(res)
+                        .toEqual(<any>[testEndpoint + 'api/geo/stations']);
                 }, done, done);
             });
         });
