d7880b23ffc93c33b1f3bb191046d8f15a78fb08
DonMahallem <donmahallem@users.noreply.github.com>
2019-04-18 13:22:09 +0200

    route not found improvements

--- src/app/modules/not-found/not-found.component.ts
+++ src/app/modules/not-found/not-found.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { Route, Router } from '@angular/router';
+import { LoadedRouterConfig } from '@angular/router/src/config';
 @Component({
     selector: 'app-not-found',
     styleUrls: ['./not-found.component.scss'],
     templateUrl: './not-found.component.pug',
 })
 export class NotFoundComponent {
-    constructor() {
-
+    constructor(private router: Router) {
     }
 
 }
