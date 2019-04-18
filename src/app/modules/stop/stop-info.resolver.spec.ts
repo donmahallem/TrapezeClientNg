5f7508c084e7e202522561566f19ae1470b509a8
DonMahallem <donmahallem@users.noreply.github.com>
2019-04-18 22:47:12 +0200

    fix tests for stop-info.resolver

--- src/app/modules/stop/stop-info.resolver.spec.ts
+++ src/app/modules/stop/stop-info.resolver.spec.ts
@@ -1,6 +1,6 @@
 import { async, TestBed } from '@angular/core/testing';
 import { Router } from '@angular/router';
-import { of, throwError, EMPTY, Observable } from 'rxjs';
+import { of, throwError, Observable } from 'rxjs';
 import { ApiService } from 'src/app/services';
 import { StopInfoResolver } from './stop-info.resolver';
 // import * as sinon from "sinon";
@@ -9,11 +9,13 @@ describe('src/app/modules/stop/stop-info.resolver', () => {
     describe('StopInfoResolver', () => {
         let resolver: StopInfoResolver;
         let getSpy: jasmine.Spy<InferableFunction>;
+        let nextSpy: jasmine.Spy<InferableFunction>;
         let navigateSpy: jasmine.Spy<InferableFunction>;
         const testId = '239jmcntest';
         beforeAll(() => {
             getSpy = jasmine.createSpy();
             navigateSpy = jasmine.createSpy();
+            nextSpy = jasmine.createSpy();
         });
         beforeEach(async(() => {
             TestBed.configureTestingModule({
@@ -35,52 +37,57 @@ describe('src/app/modules/stop/stop-info.resolver', () => {
 
         afterEach(() => {
             getSpy.calls.reset();
+            nextSpy.calls.reset();
+            navigateSpy.calls.reset();
         });
 
         describe('resolve(route, state)', () => {
             describe('should succeed', () => {
-                beforeAll(() => {
+                beforeEach(() => {
                     getSpy.and.callFake((...args: any[]): Observable<any> => {
                         return of(args);
                     });
                 });
                 it('should construct the request correctly', (done) => {
-                    resolver.resolve(<any>{ params: { stopId: testId } }, undefined).subscribe((res) => {
-                        expect(<any>res)
-                            .toEqual([<any>testId]);
-                    }, done, done);
+                    resolver.resolve(<any>{ params: { stopId: testId } }, undefined).subscribe(nextSpy, done, done);
+                });
+                afterEach(() => {
+                    expect(nextSpy.calls.count()).toEqual(1);
+                    expect(<any>nextSpy.calls.first().args[0])
+                        .toEqual([testId]);
+                    expect(navigateSpy.calls.count()).toEqual(0);
                 });
             });
             describe('should not navigate for generic error', () => {
                 const testError: Error = new Error('test error');
-                beforeAll(() => {
+                beforeEach(() => {
                     getSpy.and.callFake((...args: any[]): Observable<any> => {
                         return throwError(testError);
                     });
                 });
                 it('should construct the request correctly', (done) => {
-                    resolver.resolve(<any>{ params: { stopId: testId } }, undefined).subscribe((res) => {
-                        expect(<any>res)
-                            .toEqual([EMPTY]);
-                        expect(navigateSpy.calls.count).toEqual(0);
-                    }, done, done);
+                    resolver.resolve(<any>{ params: { tripId: testId } }, undefined).subscribe(nextSpy, done, done);
+                });
+                afterEach(() => {
+                    expect(nextSpy.calls.count()).toEqual(0);
+                    expect(navigateSpy.calls.count()).toEqual(0);
                 });
             });
-            describe('should navigate for generic error', () => {
+            describe('should navigate for 404 error', () => {
                 const testError: any = {
                     status: 404,
                 };
-                beforeAll(() => {
+                beforeEach(() => {
                     getSpy.and.callFake((...args: any[]): Observable<any> => {
                         return throwError(testError);
                     });
                 });
                 it('should construct the request correctly', (done) => {
-                    resolver.resolve(<any>{ params: { stopId: testId } }, undefined).subscribe((res) => {
-                        expect(<any>res)
-                            .toEqual([EMPTY]);
-                        expect(navigateSpy.calls.count).toEqual(1);
-                    }, done, done);
+                    resolver.resolve(<any>{ params: { tripId: testId } }, undefined).subscribe(nextSpy, done, done);
+                });
+                afterEach(() => {
+                    expect(nextSpy.calls.count()).toEqual(0);
+                    expect(navigateSpy.calls.count()).toEqual(1);
                 });
             });
         });
