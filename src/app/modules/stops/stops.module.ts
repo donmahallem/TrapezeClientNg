e006909011701d0ba41744b9f8f51b6932eea4a1
DonMahallem <donmahallem@users.noreply.github.com>
2019-04-25 22:47:44 +0200

    Init

--- src/app/modules/stops/stops.module.ts
+++ src/app/modules/stops/stops.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 // import { TripPassagesComponent } from './trip.passages.component';
 import { StopsRoutingModule } from './stops-routing.module';
 import { StopsResolver } from './stops.resolver';
+
+/**
+ * Stops lazy loaded Module
+ */
 @NgModule({
     declarations: [
         // TripPassagesComponent
--- src/app/modules/stops/stops.resolver.ts
+++ src/app/modules/stops/stops.resolver.ts
@@ -4,10 +4,24 @@ import { Observable } from 'rxjs';
 import { StationsResponse } from 'src/app/models/stations-response.model';
 import { ApiService } from '../../services';
 
+/**
+ * A Resolver for the Stations Response
+ */
 @Injectable()
 export class StopsResolver implements Resolve<StationsResponse> {
 
+    /**
+     * Constructor
+     * @param api the {@ApiService}
+     */
     public constructor(private api: ApiService) { }
+
+    /**
+     * Resolves the station response
+     * @param route The activated RouteSnapshot
+     * @param state The router state snapshot
+     * @returns An observable that resolves the {@StationsResponse}
+     */
     public resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<StationsResponse> {
         return this.api
             .getStations();
