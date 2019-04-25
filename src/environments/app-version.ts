7961aaf377fc4e716e0aff3ff48dd2b12d7611f7
donmahallem <donmahallem@users.noreply.github.com>
2019-04-25 15:27:42 +0200

    fix lint rule

--- src/environments/app-version.ts
+++ src/environments/app-version.ts
@@ -2,9 +2,11 @@
  * Reads the angular app version from the package file
  * @returns The current app version
  */
+// tslint:disable:only-arrow-functions
 export function getAppVersion(): AppVersion {
     return require('../../package.json').version;
-};
+}
+// tslint:enable:only-arrow-functions
 
 /**
  * @ignore
