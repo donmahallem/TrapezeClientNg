88a1086d11aef1f0f19066513874c70d26a9977d
donmahallem <donmahallem@users.noreply.github.com>
2019-04-25 15:14:59 +0200

    changes

--- src/environments/app-version.ts
+++ src/environments/app-version.ts
@@ -2,7 +2,7 @@
  * Reads the angular app version from the package file
  * @returns The current app version
  */
-export const getAppVersion: () => AppVersion = (): AppVersion => {
+export function getAppVersion(): AppVersion {
     return require('../../package.json').version;
 };
 
