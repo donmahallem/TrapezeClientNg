28d1e736f1705536caa03d7ae7b41245e4108426
DonMahallem <donmahallem@users.noreply.github.com>
2019-04-26 14:32:47 +0200

    Init tests for retry dialog strategy

--- src/app/rxjs-util/retry-dialog-strategy.spec.ts
+++ src/app/rxjs-util/retry-dialog-strategy.spec.ts
@@ -1,18 +1,49 @@
+import { from } from 'rxjs';
+import { retryWhen } from 'rxjs/operators';
 import { retryDialogStrategy, RetryDialogStrategyFuncResponse } from './retry-dialog-strategy';
 
 describe('src/app/rxjs-util/retry-dialog-strategy.ts', () => {
     describe('retryDialogStrategy', () => {
         let createDialogSpy: jasmine.Spy<InferableFunction>;
         let strategy: RetryDialogStrategyFuncResponse;
+        let nextSpy: jasmine.Spy<InferableFunction>;
+        let errorSpy: jasmine.Spy<InferableFunction>;
         beforeAll(() => {
             createDialogSpy = jasmine.createSpy();
+            nextSpy = jasmine.createSpy('nextSpy');
+            errorSpy = jasmine.createSpy('errorSpy');
         });
         beforeEach(() => {
             strategy = retryDialogStrategy(createDialogSpy);
         });
         afterEach(() => {
             createDialogSpy.calls.reset();
+            nextSpy.calls.reset();
+            errorSpy.calls.reset();
         });
-        it('needs to be implemented');
+        describe('No error occures', () => {
+            it('should pass', (done) => {
+                from([1, 2, 3])
+                    .pipe(retryWhen(strategy))
+                    .subscribe(nextSpy, errorSpy, () => {
+                        expect(errorSpy).not.toHaveBeenCalled();
+                        expect(nextSpy).toHaveBeenCalledTimes(3);
+                        expect(nextSpy.calls.allArgs()).toEqual([[1], [2], [3]]); // , [2], [3]);
+                        expect(createDialogSpy).not.toHaveBeenCalled();
+                        done();
+                    });
+            });
+            afterEach(() => {
+                expect(createDialogSpy).not.toHaveBeenCalled();
+            });
+        });
+        describe('Error occurs', () => {
+            describe('Should be retried', () => {
+                it('needs to be implemented');
+            });
+            describe('Should not be retried', () => {
+                it('needs to be implemented');
+            });
+        });
     });
 });
--- src/app/rxjs-util/retry-dialog-strategy.ts
+++ src/app/rxjs-util/retry-dialog-strategy.ts
@@ -10,10 +10,15 @@ export type CreateDialogFunc = (error?: ErrorItem) => CreateDialogFuncResponse;
 export type RetryDialogStrategyFuncResponse = (errors: Observable<ErrorItem>) => Observable<true>;
 export type RetryDialogStrategyFunc = (createDialog: CreateDialogFunc) => RetryDialogStrategyFuncResponse;
 
+/**
+ * If an error occurs it will call the dialog and waits for its result.
+ * If the result equals true the stream will be retried
+ * @param createDialog a method that returns valid Dialog
+ */
 export const retryDialogStrategy: RetryDialogStrategyFunc = (createDialog: CreateDialogFunc) => {
     return (errors: Observable<ErrorItem>): Observable<true> => {
         let dialogOpen = false;
-        return errors.pipe(skipWhile(() => dialogOpen),
+        return errors.pipe(skipWhile((): boolean => dialogOpen),
             flatMap((error: ErrorItem): Observable<true> => {
                 dialogOpen = true;
                 const dialogRef: CreateDialogFuncResponse = createDialog(error);
