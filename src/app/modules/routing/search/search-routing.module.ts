395b0d2b446994f34d83de796516531327e53ea9
DonMahallem <donmahallem@users.noreply.github.com>
2019-04-30 22:08:58 +0200

    search route impl

--- src/app/modules/routing/search/search-routing.module.ts
+++ src/app/modules/routing/search/search-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SearchResultResolver } from './search-result.resolver';
 import { SearchComponent } from './search.component';
 
 const tripPassagesRoute: Routes = [
     {
         component: SearchComponent,
-        path: '**',
+        path: '',
+        resolve: {
+            results: SearchResultResolver,
+        },
     },
 ];
 
--- src/app/modules/routing/search/search.component.ts
+++ src/app/modules/routing/search/search.component.ts
@@ -1,14 +1,27 @@
-import { Component, VERSION } from '@angular/core';
-import { SidebarService } from 'src/app/services/sidebar.service';
-import { environment } from 'src/environments/environment.example';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 @Component({
     selector: 'app-search',
     styleUrls: ['./search.component.scss'],
     templateUrl: './search.component.pug',
 })
-export class SearchComponent {
-    public constructor(private sidebarService: SidebarService) {
+export class SearchComponent implements OnInit, OnDestroy {
+    private searchParamSubscription: Subscription;
+    public data = '';
+    public constructor(private activatedRoute: ActivatedRoute) {
 
     }
 
+    public ngOnInit(): void {
+        this.searchParamSubscription = this.activatedRoute
+            .queryParams.subscribe((value) => {
+                this.data = JSON.stringify(value);
+            });
+    }
+    public ngOnDestroy(): void {
+        if (this.searchParamSubscription) {
+            this.searchParamSubscription.unsubscribe();
+        }
+    }
 }
