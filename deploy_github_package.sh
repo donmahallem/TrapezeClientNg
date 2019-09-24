9712e29b1211786fda2ea95c97009e75cf2980b6
donmahallem <donmahallem@users.noreply.github.com>
2019-09-24 16:37:20 +0200

    add ls to debug

--- deploy_github_package.sh
+++ deploy_github_package.sh
@@ -2,5 +2,6 @@
 echo "Creating publish config for $GITHUB_ACTOR"
 echo "//npm.pkg.github.com/:_authToken=$GITHUB_TOKEN2
 registry=https://npm.pkg.github.com/$GITHUB_ACTOR" > .npmrc
+ls
 cat ./npmrc
 npm publish
