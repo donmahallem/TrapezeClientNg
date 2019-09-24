b04090ff47a295947b116aa313a91bf414d86e10
donmahallem <donmahallem@users.noreply.github.com>
2019-09-24 16:41:25 +0200

    fix typo

--- deploy_github_package.sh
+++ deploy_github_package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "Creating publish config for $GITHUB_ACTOR"
 echo "//npm.pkg.github.com/:_authToken=$GITHUB_TOKEN2
-registry=https://npm.pkg.github.com/$GITHUB_ACTOR" > .npmrc
+registry=https://npm.pkg.github.com/$GITHUB_ACTOR" > './.npmrc'
 ls
 cat ./npmrc
 npm publish
