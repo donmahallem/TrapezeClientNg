e6a8bb1f501cafdf271512960d752fd4ffb26cc2
donmahallem <donmahallem@users.noreply.github.com>
2019-09-24 15:08:18 +0200

    bump

--- deploy_github_package.sh
+++ deploy_github_package.sh
@@ -1,3 +1,4 @@
 echo "Creating publish config for $GITHUB_ACTOR"
-echo "//npm.pkg.github.com/:_authToken=$GITHUB_TOKEN\nregistry=https://npm.pkg.github.com/$GITHUB_ACTOR" > .npmrc
+echo "//npm.pkg.github.com/:_authToken=$GITHUB_TOKEN2\nregistry=https://npm.pkg.github.com/$GITHUB_ACTOR" > .npmrc
+cat ./npmrc
 npm publish
