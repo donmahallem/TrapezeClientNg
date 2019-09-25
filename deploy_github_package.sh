2e6fac45a00a155dfc09ed46e66bafeaf06c8735
donmahallem <donmahallem@users.noreply.github.com>
2019-09-26 00:21:39 +0200

    rework script

--- deploy_github_package.sh
+++ deploy_github_package.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 echo "Creating publish config for $GITHUB_ACTOR"
-echo "//npm.pkg.github.com/:_authToken=$GITHUB_TOKEN
-registry=https://npm.pkg.github.com/$GITHUB_ACTOR" > './.npmrc'
-ls
-#cat './.npmrc'
-npm publish
-rm -rf './.npmrc'
+npm config set "//npm.pkg.github.com/:_authToken" "$GITHUB_TOKEN"
+npm publish -reg "https://npm.pkg.github.com/$GITHUB_ACTOR"
