13740311be2cbf2a3f7ba3e341dc50bc9c06b89d
donmahallem <donmahallem@users.noreply.github.com>
2019-12-02 22:27:20 +0100

    added tests

--- src/app/modules/error/not-found.component.spec.ts
+++ src/app/modules/error/not-found.component.spec.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, DebugElement, Injectable } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { BehaviorSubject } from 'rxjs';
+import { ErrorType } from './error-type';
 import { NotFoundComponent } from './not-found.component';
 
 // tslint:disable:component-selector
@@ -24,6 +28,13 @@ class TestMatListItemComponent {
 class TestMatNavListComponent {
 }
 
+@Injectable()
+class TestActivatedRoute {
+    public queryParams: BehaviorSubject<{
+        type?: any;
+    }> = new BehaviorSubject({});
+}
+
 // tslint:enable:component-selector
 // tslint:enable:directive-selector
 
@@ -31,6 +42,8 @@ describe('src/modules/error/not-found.component.ts', () => {
     describe('NotFoundComponent', () => {
         let cmpFixture: ComponentFixture<NotFoundComponent>;
         let cmp: NotFoundComponent;
+        let testActivatedRoute: TestActivatedRoute;
+        let infoBoxDebugElement: DebugElement;
         beforeEach(async(() => {
             TestBed.configureTestingModule({
                 declarations: [
@@ -42,14 +55,54 @@ describe('src/modules/error/not-found.component.ts', () => {
                 imports: [
                     RouterTestingModule,
                 ],
-                providers: [],
+                providers: [
+                    { provide: ActivatedRoute, useClass: TestActivatedRoute },
+                ],
             }).compileComponents();
             cmpFixture = TestBed.createComponent(NotFoundComponent);
             cmp = cmpFixture.debugElement.componentInstance;
+            testActivatedRoute = TestBed.get(ActivatedRoute);
+            infoBoxDebugElement = cmpFixture.debugElement.query(By.css('div.info'));
         }));
-
         it('should create the app', async(() => {
             expect(cmp).toBeTruthy();
+            expect(infoBoxDebugElement.componentInstance).toBeTruthy();
         }));
+
+        describe('error type is provided', () => {
+            describe('error type is ' + ErrorType.NotFoundPassage, () => {
+                beforeEach(() => {
+                    testActivatedRoute.queryParams.next({
+                        type: ErrorType.NotFoundPassage,
+                    });
+                    cmpFixture.detectChanges();
+                });
+                it('should only display that the passage could not be found', () => {
+                    expect(infoBoxDebugElement.nativeElement.textContent).toEqual('The passage could not be found. It either expired or has yet to start.Please select another passage.');
+                });
+            });
+            describe('error type is ' + ErrorType.NotFoundVehicle, () => {
+                beforeEach(() => {
+                    testActivatedRoute.queryParams.next({
+                        type: ErrorType.NotFoundVehicle,
+                    });
+                    cmpFixture.detectChanges();
+                });
+                it('should only display that the vehicle could not be found', () => {
+                    expect(infoBoxDebugElement.nativeElement.textContent).toEqual('The requested vehicle could not be found. It might not be active at the moment.');
+                });
+            });
+            describe('error type is an unsupported value', () => {
+                beforeEach(() => {
+                    testActivatedRoute.queryParams.next({
+                        type: -100,
+                    });
+                    cmpFixture.detectChanges();
+                });
+                it('should only display that the requested resource could not be found', () => {
+                    expect(infoBoxDebugElement.nativeElement.textContent).toEqual('The requested resource can not be found.');
+                });
+            });
+        });
     });
 });
