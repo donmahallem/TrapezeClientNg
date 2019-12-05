874416b7c118a6768813c5c45b73cda99bc9c28a
donmahallem <donmahallem@users.noreply.github.com>
2019-12-05 13:41:06 +0100

    add typings

--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { async, TestBed } from '@angular/core/testing';
+import { IStopInfo, IStopLocations, IVehicleLocation, IVehiclePathInfo } from '@donmahallem/trapeze-api-types';
 import { from, Observable } from 'rxjs';
+import { TripPassagesLocation } from '../models';
 import { ApiService } from './api.service';
 // import * as sinon from "sinon";
 describe('src/app/services/api.service', () => {
@@ -34,7 +36,7 @@ describe('src/app/services/api.service', () => {
 
         describe('getTripPassages(tripId)', () => {
             it('should construct the request correctly', (done) => {
-                apiService.getTripPassages(testId).subscribe((res) => {
+                apiService.getTripPassages(testId).subscribe((res: TripPassagesLocation) => {
                     expect(res as any)
                         .toEqual([testEndpoint + 'api/trip/' + testId + '/passages?mode=departure']);
                 }, done, done);
@@ -42,7 +44,7 @@ describe('src/app/services/api.service', () => {
         });
         describe('getRouteByVehicleId(vehicleId)', () => {
             it('should construct the request correctly', (done) => {
-                apiService.getRouteByVehicleId(testId).subscribe((res) => {
+                apiService.getRouteByVehicleId(testId).subscribe((res: IVehiclePathInfo) => {
                     expect(res)
                         .toEqual([testEndpoint + 'api/vehicle/' + testId + '/route']);
                 }, done, done);
@@ -50,7 +52,7 @@ describe('src/app/services/api.service', () => {
         });
         describe('getRouteByTripId(vehicleId)', () => {
             it('should construct the request correctly', (done) => {
-                apiService.getRouteByTripId(testId).subscribe((res) => {
+                apiService.getRouteByTripId(testId).subscribe((res: IVehiclePathInfo) => {
                     expect(res)
                         .toEqual([testEndpoint + 'api/trip/' + testId + '/route']);
                 }, done, done);
@@ -58,7 +60,7 @@ describe('src/app/services/api.service', () => {
         });
         describe('getStopInfo(vehicleId)', () => {
             it('should construct the request correctly', (done) => {
-                apiService.getStopInfo(testId).subscribe((res) => {
+                apiService.getStopInfo(testId).subscribe((res: IStopInfo) => {
                     expect(res as any)
                         .toEqual([testEndpoint + 'api/stop/' + testId + '/info']);
                 }, done, done);
@@ -74,7 +76,7 @@ describe('src/app/services/api.service', () => {
         });
         describe('getVehicleLocation(vehicleId)', () => {
             it('should construct the request correctly', (done) => {
-                apiService.getVehicleLocation(testId).subscribe((res) => {
+                apiService.getVehicleLocation(testId).subscribe((res: IVehicleLocation) => {
                     expect(res as any)
                         .toEqual([testEndpoint as any + 'api/geo/vehicle/testId1234']);
                 }, done, done);
@@ -82,7 +84,7 @@ describe('src/app/services/api.service', () => {
         });
         describe('getStations()', () => {
             it('should construct the request correctly', (done) => {
-                apiService.getStations().subscribe((res) => {
+                apiService.getStations().subscribe((res: IStopLocations) => {
                     expect(res)
                         .toEqual([testEndpoint + 'api/geo/stops'] as any);
                 }, done, done);
