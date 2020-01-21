6133ad724f710279269bb6e207220f4700bce4ae
donmahallem <donmahallem@users.noreply.github.com>
2020-01-21 17:05:43 +0100

    remove static api prefix

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -28,22 +28,22 @@ export class ApiService {
     }
 
     public getTripPassages(tripId: TripId): Observable<TripPassagesLocation> {
-        return this.http.get<TripPassagesLocation>(this.baseUrl() + 'api/trip/' + tripId + '/passages?mode=departure');
+        return this.http.get<TripPassagesLocation>(this.baseUrl() + 'trip/' + tripId + '/passages?mode=departure');
     }
     public getRouteByVehicleId(vehicleId: VehicleId): Observable<IVehiclePathInfo> {
-        return this.http.get<IVehiclePathInfo>(this.baseUrl() + 'api/vehicle/' + vehicleId + '/route');
+        return this.http.get<IVehiclePathInfo>(this.baseUrl() + 'vehicle/' + vehicleId + '/route');
     }
     public getRouteByTripId(tripId: TripId): Observable<IVehiclePathInfo> {
-        return this.http.get<IVehiclePathInfo>(this.baseUrl() + 'api/trip/' + tripId + '/route');
+        return this.http.get<IVehiclePathInfo>(this.baseUrl() + 'trip/' + tripId + '/route');
     }
     public getStopInfo(stopId: StopId): Observable<IStopInfo> {
-        return this.http.get<IStopInfo>(this.baseUrl() + 'api/stop/' + stopId + '/info');
+        return this.http.get<IStopInfo>(this.baseUrl() + 'stop/' + stopId + '/info');
     }
     public getStopPassages(stopId: StopId): Observable<IStopPassage> {
-        return this.http.get<IStopPassage>(this.baseUrl() + 'api/stop/' + stopId + '/departures');
+        return this.http.get<IStopPassage>(this.baseUrl() + 'stop/' + stopId + '/departures');
     }
     public getVehicleLocations(bounds: IMapBounds): Observable<IVehicleLocationList> {
-        return this.http.get<IVehicleLocationList>(this.baseUrl() + 'api/geo/vehicles', {
+        return this.http.get<IVehicleLocationList>(this.baseUrl() + 'geo/vehicles', {
             params: {
                 bottom: '' + Math.round(bounds.bottom * 3600000),
                 left: '' + Math.round(bounds.left * 3600000),
@@ -53,14 +53,14 @@ export class ApiService {
         });
     }
     public getVehicleLocation(vehicleId: VehicleId): Observable<IVehicleLocation> {
-        return this.http.get<IVehicleLocation>(this.baseUrl() + 'api/geo/vehicle/' + vehicleId);
+        return this.http.get<IVehicleLocation>(this.baseUrl() + 'geo/vehicle/' + vehicleId);
     }
 
     public getStations(): Observable<IStopLocations> {
-        return this.http.get<IStopLocations>(this.baseUrl() + 'api/geo/stops');
+        return this.http.get<IStopLocations>(this.baseUrl() + 'geo/stops');
     }
 
     public getSettings(): Observable<ISettings> {
-        return this.http.get<ISettings>(this.baseUrl() + 'api/settings');
+        return this.http.get<ISettings>(this.baseUrl() + 'settings');
     }
 }
