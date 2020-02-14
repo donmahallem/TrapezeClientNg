8cc8cada9772426a67714015ca2d35cb1be68723
DonMahallem <donmahallem@users.noreply.github.com>
2020-02-14 18:23:57 +0100

    add watchVehicle method

--- src/app/modules/common/map-header-box/vehicle-map-header.component.ts
+++ src/app/modules/common/map-header-box/vehicle-map-header.component.ts
@@ -1,9 +1,11 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
-import { ITripPassages, ITripRoute } from '@donmahallem/trapeze-api-types';
+import { ITripPassages, ITripRoute, IVehicleLocation } from '@donmahallem/trapeze-api-types';
 import * as L from 'leaflet';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { of, BehaviorSubject, Observable } from 'rxjs';
+import { distinctUntilChanged, map, switchMap } from 'rxjs/operators';
+import { LeafletUtil } from 'src/app/leaflet';
 import { ApiService, TripInfoWithId } from 'src/app/services';
-import { VehicleService } from 'src/app/services/vehicle.service';
+import { TimestampedVehicleLocation, VehicleService } from 'src/app/services/vehicle.service';
 import { MapHeaderComponent } from './map-header.component';
 
 export interface IVehicleInfo {
@@ -41,7 +43,6 @@ export class VehicleMapHeaderBoxComponent extends MapHeaderComponent {
                        public apiService: ApiService) {
         super();
         this.tripSubject = new BehaviorSubject(undefined);
-        /*
         this.tripObservable = this.tripSubject.asObservable()
             .pipe(distinctUntilChanged((x: ITripPassages, y: ITripPassages): boolean => {
                 if (x && y) {
@@ -49,39 +50,22 @@ export class VehicleMapHeaderBoxComponent extends MapHeaderComponent {
                 }
                 return false;
             }));
-        this.vehicleLocationObservable = this.tripObservable
-            .pipe(tap(() => {
-                this.lastUpdate = new Date();
-            }), switchMap((trip: ITripPassages): Observable<any> => {
-                if (trip) {
-                    return this.vehicleService
-                        .getVehicleByTripId(trip.tripId)
-                        .pipe(catchError((err: any): Observable<void> =>
-                            EMPTY));
-                } else {
-                    return of(undefined);
-                }
-            }),
-                // needed for pairwise not delaying the display
-                startWith(undefined as TimestampedVehicleLocation),
-                pairwise(),
-                map((val: [TimestampedVehicleLocation, TimestampedVehicleLocation]): IVehicleInfo => {
-                    if (val[0] && val[1]) {
-                        const loc1: L.LatLng = LeafletUtil.convertCoordToLatLng(val[1]);
-                        const loc2: L.LatLng = LeafletUtil.convertCoordToLatLng(val[0]);
-                        const dst: number = loc1.distanceTo(loc2) / 1000;
-                        const tDst: number = val[1].lastUpdate - val[0].lastUpdate;
-                        const vehicleSpeed: number = tDst > 0 ? (dst / tDst / 3600) : -1;
-                        return {
-                            location: LeafletUtil.convertCoordToLatLng(val[1]),
-                            speed: vehicleSpeed,
-                        };
-                    }
-                    return {
-                        location: val[1] ? LeafletUtil.convertCoordToLatLng(val[1]) : L.latLng(0, 0),
-                        speed: -1,
-                    };
-                }));*/
     }
 
+    public watchVehicle(source: Observable<TripInfoWithId>): Observable<L.LatLng> {
+        return source.pipe(switchMap((trip: TripInfoWithId): Observable<TimestampedVehicleLocation> => {
+            if (trip) {
+                return this.vehicleService
+                    .getVehicleByTripId(trip.tripId);
+            } else {
+                return of(undefined);
+            }
+        }), distinctUntilChanged((x: TimestampedVehicleLocation, y: TimestampedVehicleLocation) => {
+            if (x && y) {
+                return x.lastUpdate === y.lastUpdate;
+            }
+            return false;
+        }), map((loc: IVehicleLocation): L.LatLng =>
+            loc ? LeafletUtil.convertCoordToLatLng(loc) : undefined));
+    }
 }
