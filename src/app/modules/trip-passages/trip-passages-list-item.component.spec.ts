0b753a468a71ce728bc25b36c8b0f348e96d8523
donmahallem <donmahallem@users.noreply.github.com>
2020-02-14 02:07:52 +0100

    added tests for getters in TripPassagesListItemComponent

--- src/app/modules/trip-passages/trip-passages-list-item.component.spec.ts
+++ src/app/modules/trip-passages/trip-passages-list-item.component.spec.ts
@@ -80,7 +80,40 @@ describe('src/app/modules/trip-passages/trip-passages-list-item.component', () =
       }).compileComponents();
     }));
     describe('properties', () => {
-      it('needs to be implemented');
+      let cmpFixture: ComponentFixture<TripPassagesListItemComponent>;
+      let cmp: TripPassagesListItemComponent;
+      beforeEach(() => {
+        cmpFixture = TestBed.createComponent(TripPassagesListItemComponent);
+        cmp = cmpFixture.componentInstance;
+      });
+      describe('departed', () => {
+        // tslint:disable-next-line:no-null-keyword
+        [null, undefined, { status: VEHICLE_STATUS.STOPPING }]
+          .forEach((testValue: any) => {
+            it('should return false for :' + JSON.stringify(testValue), () => {
+              cmp.passage = testValue;
+              expect(cmp.departed).toBeFalse();
+            });
+          });
+        it('should return true for status being DEPARTED ', () => {
+          cmp.passage = { status: VEHICLE_STATUS.DEPARTED } as ITripPassage;
+          expect(cmp.departed).toBeTrue();
+        });
+      });
+      describe('stopping', () => {
+        // tslint:disable-next-line:no-null-keyword
+        [null, undefined, { status: VEHICLE_STATUS.DEPARTED }]
+          .forEach((testValue: any) => {
+            it('should return false for :' + JSON.stringify(testValue), () => {
+              cmp.passage = testValue;
+              expect(cmp.stopping).toBeFalse();
+            });
+          });
+        it('should return true for status being STOPPING', () => {
+          cmp.passage = { status: VEHICLE_STATUS.STOPPING } as ITripPassage;
+          expect(cmp.stopping).toBeTrue();
+        });
+      });
     });
     describe('without parent element', () => {
       let cmpFixture: ComponentFixture<TripPassagesListItemComponent>;
